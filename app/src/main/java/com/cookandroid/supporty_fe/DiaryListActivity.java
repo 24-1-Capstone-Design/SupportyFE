0474e8c07504bd27aad9a82001018e71228b73b4
MinJeong <20190018@sungshin.ac.kr>
2024-05-10 13:57:34 +0900

    feat: DiaryListActivity

---
+++ app/src/main/java/com/cookandroid/supporty_fe/DiaryListActivity.java
@@ -0,0 +1,38 @@
+package com.cookandroid.supporty_fe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import java.util.ArrayList;
+
+public class DiaryListActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> diaryList;
+
+    @SuppressLint("MissingInflatedId")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_list);
+
+        listView = findViewById(R.id.diary_list);
+        diaryList = new ArrayList<>();
+
+        // 여기서 postList에 사용자의 댓글을 추가
+        diaryList.add("240306 일기");
+        diaryList.add("240302 일기");
+        diaryList.add("240301");
+        diaryList.add("240221");
+        diaryList.add("240123");
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, diaryList);
+        listView.setAdapter(adapter);
+    }
+}
